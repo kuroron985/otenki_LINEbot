08acbc99dec0c3599652a3ce4c98c694472db073
kuroron985 <shigotoyou.taka@gmil.com>
2020-03-09 02:56:36 +0900

    [add] コントローラー作成時に自動生成しないファイルを設定

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,19 @@ module OtenkiLinebot
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
